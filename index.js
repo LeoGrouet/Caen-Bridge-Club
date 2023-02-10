4fcc5d60ece5b8860fb2a9c2eee1a8fe35a23b93
Léo Grouet <leo.grouet@gmail.com>
2023-02-10 18:35:01 +0100

    Add .gitignore / rework de la page principale + mise en place serveur express

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-const express = require("express");
-const router = require("router");
+const express = require('express')
+const app = express()
+const port = 3000
 
-const app = express();
-app.use(express.json());
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server Listening on ${process.env.HOST}:${process.env.PORT}`);
-  });
+app.use(express.static('client'));
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
